c9e85166615e364734ccf86782c9ffba2d13e995
Antonio Barrera <jeantonio.barrera@gmail.com>
2021-10-04 22:10:35 -0600

    ...

--- src/main/java/ventanas/VentanaPokedex.java
+++ src/main/java/ventanas/VentanaPokedex.java
@@ -420,7 +420,7 @@ public class VentanaPokedex extends javax.swing.JFrame {
                     .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblColor))
                 .addGap(28, 28, 28)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(izq)
                     .addComponent(der))
                 .addGap(25, 25, 25))
